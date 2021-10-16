205f4a0f6b811bfd21f793e3ed3b71f4caa4741c
Pedro Rey Anca <pedroreya@gmail.com>
2021-10-16 16:23:24 +0200

    Solve wrong naming with name and id

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ with open(SETTINGS_FILE, 'r') as f:
 
 app = Flask(__name__)
 
-@app.route('/unlock/<device_name>', methods=['GET', 'POST']) #device_name is the name of the device in configuration.yaml
-def unlock_luks_device(device_name):
+@app.route('/unlock/<device_id>', methods=['GET', 'POST']) #device_id is the id of the device in settings.yaml
+def unlock_luks_device(device_id):
     key = request.args.get('key')
-    device_settings = settings["devices"][device_name]
+    device_settings = settings["devices"][device_id]
 
-    with open(f"/tmp/{device_name}.key", 'w') as f:
+    with open(f"/tmp/{device_settings.get('name')}.key", 'w') as f:
         f.write(key)
 
-    cryptsetup_result = subprocess.run(["cryptsetup", "luksOpen", device_settings.get('path'), device_settings.get('name'), "-d", f"/tmp/{device_name}.key"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    cryptsetup_result = subprocess.run(["cryptsetup", "luksOpen", device_settings.get('path'), device_settings.get('name'), "-d", f"/tmp/{device_settings.get('name')}.key"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     cryptsetup_output = cryptsetup_result.stdout.decode("utf-8")
     mount_result = subprocess.run(["mount", f"/dev/mapper/{device_settings.get('name')}", device_settings.get('mount')], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     mount_output = mount_result.stdout.decode("utf-8")
