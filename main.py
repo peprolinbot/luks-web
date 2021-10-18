5a010d1ec6595390e9111429baf4cdf104494ef3
Pedro Rey Anca <pedroreya@gmail.com>
2021-10-18 16:12:49 +0200

    Add UI. Includes endpoint to check if device is unlocked

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import subprocess
-from flask import Flask, request
+import os
+from flask import Flask, request,render_template
 import yaml
 
 SETTINGS_FILE="settings.yaml"
@@ -9,10 +10,47 @@ with open(SETTINGS_FILE, 'r') as f:
 
 app = Flask(__name__)
 
+@app.route('/')
+def unlock_page():
+    password_forms = []
+    for device_id, device in settings["devices"].items():
+        password_forms.append(f"""
+        <form id={device_id}_form action="/unlock/{device_id}" method="POST" target="hidden-form">
+            <label for="{device_id}">Passphrase for {device.get("name")}:</label>
+            <input type="password" id="{device_id}" name="{device_id}" value="">
+            <label for="{device_id}" id="{device_id}_status">❌</label>
+            <input type="submit" id="{device_id}_submit" value="Submit">
+            <script>
+                function reqListener () {{
+                    const response = JSON.parse(this.responseText);
+                    if ( response.status == "unlocked" ) {{
+                        {device_id}.disabled = true;
+                        {device_id}_submit.style.display = "none";
+                        {device_id}_status.innerHTML = "✅";
+                    }}
+                }}
+
+                function checkUnlocked() {{
+                    var target = '/check_unlocked/{device_id}';
+                    var xhr = new XMLHttpRequest();
+                    xhr.addEventListener("load", reqListener);
+                    xhr.open('GET', target);
+                    xhr.send();
+                }}
+
+                checkUnlocked();
+                setInterval(checkUnlocked, 5000);
+            </script>
+        </form>
+        """) # Double curlies are for escaping
+    password_forms_str = "\n".join(password_forms)
+    return render_template('unlock.html',
+                           password_forms=password_forms_str)
+
 @app.route('/unlock/<device_id>', methods=['GET', 'POST']) #device_id is the id of the device in settings.yaml
 def unlock_luks_device(device_id):
     device_settings = settings["devices"][device_id]
-    key = request.args.get('key')
+    key =  request.form.get(device_id) or request.args.get('key')
     key_file = f"/tmp/{device_settings.get('name')}.key"
 
     with open(f"/tmp/{device_settings.get('name')}.key", 'w') as f:
@@ -39,7 +77,14 @@ def unlock_luks_device(device_id):
                 "mount_output": mount_output}
 
 
+@app.route('/check_unlocked/<device_id>', methods=['GET']) #device_id is the id of the device in settings.yaml
+def check_unlocked_luks_device(device_id):
+    device_settings = settings["devices"][device_id]
 
+    if os.path.exists(f"/dev/mapper/{device_settings.get('name')}"):
+        return {"status": "unlocked"}
+    else:
+        return {"status": "locked"}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
