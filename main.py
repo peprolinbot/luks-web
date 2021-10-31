b9ab309a22d25e1a550d6b05ac4735062eec111e
Pedro Rey Anca <pedroreya@gmail.com>
2021-10-31 17:18:16 +0100

    Move form template from main.py to html

--- main.py
+++ main.py
@@ -14,46 +14,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def unlock_page():
-    password_forms = []
-    for device_id, device in settings["devices"].items():
-        password_forms.append(f"""
-        <form id={device_id}_form action="/unlock/{device_id}" method="POST" target="hidden-form">
-            <label for="{device_id}_password">Passphrase for {device.get("name")}:</label>
-            <input type="password" id="{device_id}_password" name="{device_id}_password" value="">
-            <label for="{device_id}_password" id="{device_id}_status">❌</label>
-            <input type="submit" id="{device_id}_submit" value="Submit">
-            <script>
-                function {device_id}_reqListener () {{
-                    const response = JSON.parse(this.responseText);
-                    if ( response.status == "unlocked" ) {{
-                        clearInterval({device_id}_checkUnlockedInterval);
-                        {device_id}_password.disabled = true;
-                        {device_id}_submit.style.display = "none";
-                        {device_id}_status.innerHTML = "✅";
-                        encryptedDevices -= 1;
-                        if ( encryptedDevices <= 0 ) {{
-                            setTimeout("window.location.href = '/unlocked'", 2000);
-                        }}
-                    }}
-                }}
-
-                function {device_id}_checkUnlocked() {{
-                    var target = '/check_unlocked/{device_id}';
-                    var xhr = new XMLHttpRequest();
-                    xhr.addEventListener("load", {device_id}_reqListener);
-                    xhr.open('GET', target);
-                    xhr.send();
-                }}
-
-                {device_id}_checkUnlocked();
-                var {device_id}_checkUnlockedInterval = setInterval({device_id}_checkUnlocked, 5000);
-            </script>
-        </form>
-        """) # Double curlies are for escaping
-    password_forms_str = "\n".join(password_forms)
+    devices = settings["devices"].items()
     return render_template('unlock.html',
-                           password_forms=password_forms_str,
-                           encrypted_devices=len(password_forms))
+                           devices=devices,
+                           encrypted_devices=len(devices))
 
 @app.route('/unlocked')
 def unlocked_page():
